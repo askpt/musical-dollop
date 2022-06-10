11de0ea9b768e50e337d24ce0e17edcebc81fa7d
Restyled.io <commits@restyled.io>
2022-06-10 14:17:35 +0000

    Restyled by clang-format

--- src/hackAWeather/Program.cs
+++ src/hackAWeather/Program.cs
@@ -10,31 +10,29 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 var summaries =
     new[] { "Freezing", "Bracing", "Chilly", "Cool",       "Mild",
-            "Warm",     "Balmy",   "Hot",    "Sweltering", "Scorching"
-          };
+            "Warm",     "Balmy",   "Hot",    "Sweltering", "Scorching" };
 
 app.MapGet("/weatherforecast", async () => {
-    var forecast =
-        Enumerable.Range(1, 5)
-        .Select(index => new WeatherForecast(
-        DateTime.Now.AddDays(index),
-        Random.Shared.Next(-20, 55),
-        summaries[Random.Shared.Next(summaries.Length)]))
-        .ToArray();
-    await Task.Delay(250);
-    return forecast;
-}).WithName("GetWeatherForecast");
+     var forecast =
+         Enumerable.Range(1, 5)
+             .Select(index => new WeatherForecast(
+                         DateTime.Now.AddDays(index),
+                         Random.Shared.Next(-20, 55),
+                         summaries[Random.Shared.Next(summaries.Length)]))
+             .ToArray();
+     await Task.Delay(250); return forecast;
+   }).WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    record WeatherForecast(DateTime Date, int TemperatureC, string? Summary) {
+      public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
