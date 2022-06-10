fe7c0083796da41bbbccb442db2d7d3ec591c83c
Michele Scandura <michele.scandura@lexisnexisrisk.com>
2022-06-10 14:22:43 +0100

    add thresholds to k6

--- loadSpikeSoakScript.js
+++ loadSpikeSoakScript.js
@@ -1,6 +1,10 @@
 import http from 'k6/http';
 import { check } from 'k6';
 export const options = {
+	thresholds: {
+    	http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    	http_req_duration: ['p(95)<5'], // 95% of requests should be below 200ms
+  	},
     scenarios: {
         load_testing: {
             executor: 'ramping-arrival-rate',
