392e8a3fc9261d7f8973d549cc2afbe48d062186
Piotr Justyna <piotr.justyna@gmail.com>
2022-06-10 15:25:56 +0100

    Update Program.cs

--- src/hackAWeather/Program.cs
+++ src/hackAWeather/Program.cs
@@ -28,7 +28,7 @@ app.MapGet("/weatherforecast", async () => {
                          Random.Shared.Next(-20, 55),
                          summaries[Random.Shared.Next(summaries.Length)]))
              .ToArray();
-     await Task.Delay(250);
+     await Task.Delay(1);
      return forecast;
    }).WithName("GetWeatherForecast");
 
